65392097186cb7d828693e236fe3a4af5641bd7f
Alan Zhang <zhang.alan@northeastern.edu>
2023-12-05 04:08:08 -0500

    Merge into main

---
+++ cmake-build-debug/CMakeFiles/FrugallyDeep.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jiezhang/Desktop/CS3520/final-project-cs3520/frugally-deep/lib/src/Model.cpp" "CMakeFiles/FrugallyDeep.dir/lib/src/Model.cpp.o" "gcc" "CMakeFiles/FrugallyDeep.dir/lib/src/Model.cpp.o.d"
+  "/Users/jiezhang/Desktop/CS3520/final-project-cs3520/frugally-deep/main.cpp" "CMakeFiles/FrugallyDeep.dir/main.cpp.o" "gcc" "CMakeFiles/FrugallyDeep.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FrugallyDeep.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for FrugallyDeep.
+# This may be replaced when dependencies are built.
